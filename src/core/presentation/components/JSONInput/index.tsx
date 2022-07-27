23c625f1ffcf97ef1f51b456092e060ff8c6d5ff
Hillel Saal <remi.saal@gmail.com>
2022-07-27 20:26:16 +0300

    fix: JSONInput

--- src/core/presentation/components/JSONInput/index.tsx
+++ src/core/presentation/components/JSONInput/index.tsx
@@ -1,53 +1,34 @@
 import React from 'react'
+import AceEditor from 'react-ace'
+
+import 'ace-builds/src-noconflict/mode-json'
+import 'ace-builds/src-noconflict/ext-language_tools'
 
 import * as classes from './JSONInput.module.scss'
-import Editor, { useMonaco } from '@monaco-editor/react'
 
 interface JSONInputProps {
     value: string
     onChange: (value: string) => void
     label?: string
-    error?: string
 }
 
-const JSONInput: React.FC<JSONInputProps> = ({
-    value,
-    onChange,
-    label,
-    error,
-}) => {
-    const monaco = useMonaco()
-
-    if (monaco !== null) {
-        console.log(monaco.editor)
-    }
-
+const JSONInput: React.FC<JSONInputProps> = ({ value, onChange, label }) => {
     return (
         <div className={classes.wrapper}>
             {label && <label className={classes.label}>{label}:</label>}
             <div className={classes['editor-wrapper']}>
-                <Editor
+                <AceEditor
                     className={classes.editor}
-                    language="json"
-                    theme={'vs-dark'}
-                    options={{
-                        automaticLayout: true,
-                    }}
-                    // fontSize="1rem"
-                    // enableSnippets={true}
-                    // setOptions={{
-                    //     enableBasicAutocompletion: true,
-                    //     enableLiveAutocompletion: true,
-                    // }}
-                    // highlightActiveLine={false}
-                    // minLines={3}
-                    // maxLines={10}
-                    // showPrintMargin={false}
+                    mode={'json'}
+                    fontSize={'1rem'}
+                    minLines={3}
+                    maxLines={10}
+                    showGutter={false}
+                    showPrintMargin={false}
                     value={value}
-                    onChange={value => onChange(value!)}
+                    onChange={value => onChange(value)}
                 />
             </div>
-            <small className={classes.error}>{error}</small>
         </div>
     )
 }
